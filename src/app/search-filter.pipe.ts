b2d20009ec5f9262d2fd0c9e5cf389ad05ba0454
LEBLANC <adrien.leblanc@capgemini.com>
2019-09-10 12:00:40 +0200

    Ajout du pipe filter

---
+++ src/app/search-filter.pipe.ts
@@ -0,0 +1,22 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { Wine } from './model/wine';
+
+@Pipe({
+  name: 'filter'
+})
+
+@Injectable()
+export class SearchFilterPipe implements PipeTransform {
+
+  transform(data: any, filters: any): any {
+    if (!data) return data;
+    return data.filter(item => {
+      return (item.appellation.toLowerCase().indexOf(filters.appellation.toLowerCase()) >= 0
+      && item.nomChateau.toLowerCase().indexOf(filters.nomChateau.toLowerCase()) >= 0
+      && item.type.toLowerCase().indexOf(filters.type.toLowerCase()) >= 0
+      && item.millesime.toString().indexOf(filters.millesime.toLowerCase()) >= 0
+      );
+    });
+  }
+
+}
